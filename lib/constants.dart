d667dd5f553d7ce56ff3d85956488d709b978c5a
cizodevahm <93611338+cizodevahm@users.noreply.github.com>
2023-02-11 00:14:16 +0530

    Commit of lib folder

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+class Constant {
+  static String fcmToken = '';
+  static String deviceToken = '';
+  static String token = '';
+  static int? currentUserId;
+static String  newUser = "";
+  static String accessToken = "";
+}
